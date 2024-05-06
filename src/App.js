e9f522e4a1c54ca70a104331f9e1b2af75aa5d01
Simon Post <simon.post.1999@gmail.com>
2024-05-06 18:33:25 -0400

    start of akinator created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import akinator from './akinator.png'
 
 function App() {
+  let questions = [
+    {
+      "question": "What is your phone number's favorite color?",
+      "options": [
+        "Green",
+        "Yellow",
+        "Red",
+        "Purple"
+      ]
+    },
+    {
+      "question": "Where is your phone number from?",
+      "options": [
+        "North America",
+        "South America",
+        "France",
+        "Asia"
+      ]
+    }
+  ]
+  
+  const [currentQuestion, setCurrentQuestion] = useState({})
+
+  useEffect(() => {
+    let randomQuestionIndex = Math.floor(Math.random() * questions.length);
+    setCurrentQuestion(questions[randomQuestionIndex])
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h3 className='header'>Allow Akinator to guess your phone number</h3>
+      <div className='mainContent'>
+        <img src={akinator} className='akinatorImage'/>
+        <div className='questionBox'>
+          <h4 className='questionText'>{currentQuestion.question}</h4>
+          <div className='optionsList'>
+            {currentQuestion.options.map(item => (
+              <button className='option'>{item}</button>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
